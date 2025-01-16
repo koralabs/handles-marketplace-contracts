3ffec1747a2f22ec225cc351cbb0a52705d426f9
Golddy Dev <firingdev@gmail.com>
2025-01-17 02:26:43 +0800

    add iutxo-txinput function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,19 @@
+import { hexToBytes } from "@helios-lang/codec-utils";
+import {
+  makeAddress,
+  makeAssets,
+  makeInlineTxOutputDatum,
+  makeTxInput,
+  makeTxOutput,
+  makeTxOutputId,
+  makeValue,
+  TxInput,
+} from "@helios-lang/ledger";
+import { decodeUplcData } from "@helios-lang/uplc";
+import { IUTxO } from "@koralabs/kora-labs-common";
+
+import { HANDLE_POLICY_ID } from "../constants/index.js";
+
 const bigIntMin = (...args: bigint[]): bigint => {
   return args.reduce((min, e) => {
     return e < min ? e : min;
@@ -14,4 +30,24 @@ const sleep = async (milliseconds: number): Promise<void> => {
   await new Promise((resolve) => setTimeout(() => resolve(true), milliseconds));
 };
 
-export { bigIntMax, bigIntMin, sleep };
+const makeListingTxInputFromListingIUTxO = (
+  listingIUtxo: IUTxO,
+  handleHex: string
+): TxInput =>
+  makeTxInput(
+    makeTxOutputId(`${listingIUtxo.tx_id}#${listingIUtxo.index}`),
+    makeTxOutput(
+      makeAddress(listingIUtxo.address),
+      makeValue(
+        BigInt(listingIUtxo.lovelace),
+        makeAssets([[HANDLE_POLICY_ID, [[handleHex, 1n]]]])
+      ),
+      listingIUtxo.datum
+        ? makeInlineTxOutputDatum(
+            decodeUplcData(hexToBytes(listingIUtxo.datum))
+          )
+        : undefined
+    )
+  );
+
+export { bigIntMax, bigIntMin, makeListingTxInputFromListingIUTxO, sleep };
